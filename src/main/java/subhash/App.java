ec7bd2a67d197713dd6b4e82da59ef59c9a4c561
subhash <subhash.shivangi26@gmail.com>
2026-07-17 16:16:37 +0530

    MVN source file added

---
+++ src/main/java/subhash/App.java
@@ -0,0 +1,13 @@
+package subhash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
